0555c3eee60f20abb485358bdd908eeeb31005c7
cuiy0006 <yc2554@nyu.edu>
2020-11-18 00:32:26 -0500

    add build

---
+++ build.sh
@@ -0,0 +1,7 @@
+g++ -Wall -std=c++17 020\ Hello\ World.cpp -o 020
+g++ -Wall -std=c++17 -pthread 030\ Basic\ Multithreading.cpp -o 030
+g++ -Wall -std=c++17 -pthread  050\ Shared\ Data.cpp -o 050
+g++ -Wall -std=c++17 -pthread 060\ Mutexes.cpp -o 060
+g++ -Wall -std=c++17 -pthread 065\ Function\ Arguments.cpp -o 065
+g++ -Wall -std=c++17 -pthread 070\ Lock\ Guards.cpp -o 070
+g++ -Wall -std=c++17 -pthread 080\ Threads\ with\ Callable\ Objects.cpp -o 080
