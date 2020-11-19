2f2865d458917cc6b2bddcb8ec8295442e2572c9
Yao Cui <cuiy0006@Yaos-MacBook-Pro.local>
2020-11-19 03:19:03 -0500

    .

--- 190 Producer Consumer.cpp
+++ 190 Producer Consumer.cpp
@@ -15,6 +15,7 @@ private:
     int _max_size;
     queue<E> _queue;
 
+
 public:
     blocking_queue(int max_size): _max_size(max_size)
     {
@@ -33,29 +34,23 @@ public:
         _cond.notify_one();
     }
 
-    E front()
-    {
-        unique_lock<mutex> lock(_mtx);
-         _cond.wait(lock, [this](){ return !_queue.empty(); });
-
-        return _queue.front();
-    }
-
-    void pop()
+    E pop()
     {
         unique_lock<mutex> lock(_mtx);
 
         _cond.wait(lock, [this](){ return !_queue.empty(); });
 
+        E item = _queue.front();
         _queue.pop();
 
         lock.unlock();
         _cond.notify_one();
+
+        return item;
     }
 
     int size()
     {
-        lock_guard<mutex> lock(_mtx);
         return _queue.size();
     }
 };
@@ -73,27 +68,16 @@ int main()
         }
     });
 
-    thread t3([&](){
-        for(int i = 10; i < 20; i++)
-        {
-            cout << "pushing " << i << endl;
-            cout << "queue size is " << qu.size() << endl;
-            qu.push(i);
-        }
-    });
-
     thread t2([&](){
-        for(int i = 0; i < 20; i++)
+        for(int i = 0; i < 10; i++)
         {
-            auto item = qu.front();
-            qu.pop();
+            auto item = qu.pop();
             cout << "consumed " << item << endl;
         }
     });
 
     t1.join();
     t2.join();
-    t3.join();
     
     return 0;
 }
